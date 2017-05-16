e9638b8fbbdcbf7ffe73e1e0364f46727d1f5cc9
Ruinan Zhang <zhangr4@rose-hulman.edu>
2017-05-16 00:21:22 -0400

    fix p1

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -197,13 +197,15 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent,learning=True,epsilon = 1, alpha = 0.95)
+    # agent = env.create_agent(LearningAgent,learning=True,epsilon = 1, alpha = 0.95)
+    agent = env.create_agent(LearningAgent)
     
     ##############
     # Follow the driving agent
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
-    env.set_primary_agent(agent,enforce_deadline=True)
+    # env.set_primary_agent(agent,enforce_deadline=True)
+    env.set_primary_agent(agent)
 
     ##############
     # Create the simulation
@@ -212,14 +214,16 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True, optimized=True)
+    # sim = Simulator(env, update_delay=0.01, log_metrics=True, optimized=True)
+    sim = Simulator(env)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10, tolerance=0.0001)
+    # sim.run(n_test=10, tolerance=0.0001)
+    sim.run()
 
 
 if __name__ == '__main__':
